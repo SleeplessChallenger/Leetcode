c88bc7ff311c89fceeaaecc7d76696f6c6962850
Daniil Slobodeniuk <76875535+SleeplessChallenger@users.noreply.github.com>
2021-08-14 09:25:19 +0500

    Update sorting_searching.py

--- TopInterviewQuestions/Easy/sorting_searching.py
+++ TopInterviewQuestions/Easy/sorting_searching.py
@@ -24,4 +24,42 @@ class Solution:
                     n -= 1
             idx -= 1
 
-# 2. 
+# 2. First Bad Version
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return an integer
+# def isBadVersion(version):
+
+class Solution:
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        # Time: O(n)
+        if n == 0:
+            return 0
+        if n == 1:
+            if isBadVersion(n):
+                return 1
+        
+        for i in range(1, n + 1):
+            if isBadVersion(i) == True:
+                return i
+        
+        # Optimal. As we're said that after one bad
+        # version we get all bad -> use binary search 
+        # to trim useless parts
+        left = 1
+        right = n
+        while left <= right:
+            middle = (left + right) // 2
+            if isBadVersion(middle) == True:
+                right = middle - 1
+            # if there is bad to the right
+            # => move left to find bad
+   
+            else:
+                left = middle + 1
+        
+        return left
