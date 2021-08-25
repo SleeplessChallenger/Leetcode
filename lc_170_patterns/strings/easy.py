865f3be453104af7630f7c1ee214bd60307d6420
Daniil Slobodeniuk <76875535+SleeplessChallenger@users.noreply.github.com>
2021-08-25 08:50:50 +0500

    Update easy.py

--- lc_170_patterns/strings/easy.py
+++ lc_170_patterns/strings/easy.py
@@ -22,3 +22,66 @@ class Solution:
         
     def isGreater(self, letter, target):
         return ord(letter) > ord(target)
+
+
+# 2. Backspace String Compare
+class Solution:
+    # Time: O(m + n) Space: O(m + n)
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        stack1 = []
+        for i in s:
+            if i != '#':
+                stack1.append(i)
+            elif len(stack1) != 0:
+                stack1.pop()
+        
+        stack2 = []
+        for j in t:
+            if j != '#':
+                stack2.append(j)
+            elif len(stack2) != 0:
+                stack2.pop()
+        
+        return ''.join(stack1) == ''.join(stack2)
+    
+    # Time: O(m + n) Space: O(1)
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        countS = 0
+        countT = 0
+        
+        i = len(s) - 1
+        j = len(t) - 1
+        
+        while i >= 0 or j >= 0:
+            while i >= 0:
+                if s[i] == '#':
+                    countS += 1
+                    i -= 1
+                elif countS > 0:
+                    countS -= 1
+                    i -= 1
+                else:
+                    break
+            
+            while j >= 0:
+                if t[j] == '#':
+                    countT += 1
+                    j -= 1
+                elif countT > 0:
+                    countT -= 1
+                    j -= 1
+                else:
+                    break
+            
+            # at first valid indicies check
+            # as we can get error otherwise
+            if i >= 0 and j >= 0 and s[i] != t[j]:
+                return False
+            
+            if (i >= 0) != (j >= 0):
+                return False
+            
+            i = i - 1
+            j = j - 1
+        
+        return True
