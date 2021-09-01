f89dce3071ed9ad751df9cd69bc2234d2d74119a
Daniil Slobodeniuk <76875535+SleeplessChallenger@users.noreply.github.com>
2021-09-01 10:15:52 +0500

    Update searching.py

--- Blind_75/searching.py
+++ Blind_75/searching.py
@@ -26,3 +26,38 @@ class Solution:
                     right = middle - 1
         
         return -1
+
+# 2.  Find Minimum in Rotated Sorted Array
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        if len(nums) < 2:
+            return nums[0]
+        
+        left = 0
+        right = len(nums) - 1
+        
+        if nums[right] > nums[left]:
+            # no rotation
+            return nums[left]
+        
+        while left <= right:
+            middle = (left + right) // 2
+            
+            # as even with `[4, 3]` middle is
+            # to the left (4 in this case) =>
+            # middle + 1 is safe
+            if nums[middle] > nums[middle + 1]:
+                return nums[middle + 1]
+            # hence middle is smaller than next one
+            # and if it's smaller than prev => return
+            # here middle being 0 becomes -1
+            # in `nums[middle - 1]`
+            elif nums[middle] < nums[middle - 1]:
+                return nums[middle]
+            
+            elif nums[middle] > nums[left]:
+                # go right as before middle everything is growing
+                left = middle + 1
+            elif nums[middle] < nums[right]:
+                # go left as after middle all increase
+                right = middle - 1
