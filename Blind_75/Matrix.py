d43050bb574e74d2109ab18a0eedb809e4852b5f
Daniil Slobodeniuk <76875535+SleeplessChallenger@users.noreply.github.com>
2021-09-06 08:33:53 +0500

    Update Matrix.py

--- Blind_75/Matrix.py
+++ Blind_75/Matrix.py
@@ -45,3 +45,76 @@ class Solution:
             e_c -= 1
         
         return result
+
+# 2 Set Matrix Zeroes
+class Solution:
+    # Mine (not-optimal)
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        
+        # flipped values are not considered as zeros
+        real_zeros = []
+        self.findRealZeros(matrix, real_zeros)
+
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                figure = matrix[i][j]
+                
+                if figure != 0:
+                    continue
+                
+                if [i, j] not in real_zeros:
+                    continue
+                    
+                self.flipValues(matrix, i, j)
+    
+    def flipValues(self, matrix, i, j):
+        for a in range(len(matrix[i])):
+            matrix[i][a] = 0
+        
+        for b in range(len(matrix)):
+            matrix[b][j] = 0
+        
+    def findRealZeros(self, matrix, real_zeros):
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                figure = matrix[i][j]
+                if figure != 0:
+                    continue
+
+                real_zeros.append([i, j])
+
+    # Optimal (non-mine)
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        is_col = False
+        for i in range(len(matrix)):
+            if matrix[i][0] == 0:
+                # if we need to set 0
+                # for first column
+                is_col = True
+            
+            # start with second row so as
+            # not to mess first row as we can
+            # simply check first row in the end
+            for j in range(1, len(matrix[i])):
+                if matrix[i][j] == 0:
+                    matrix[i][0] = 0
+                    matrix[0][j] = 0
+        
+        # Traverse from second row & col and if
+        # first row or col was marked (0 is False in bool
+        # hence `not` is a nice check)
+        for i in range(1, len(matrix)):
+            for j in range(1, len(matrix[i])):
+                if not matrix[i][0] or not matrix[0][j]:
+                    matrix[i][j] = 0
+        
+        if matrix[0][0] == 0:
+            for j in range(len(matrix[0])):
+                matrix[0][j] = 0
+        
+        if is_col:
+            for i in range(len(matrix)):
+                matrix[i][0] = 0
