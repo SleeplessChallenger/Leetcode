b0208dd720454fdac21c5da116de54fcab0cfa81
Daniil Slobodeniuk <76875535+SleeplessChallenger@users.noreply.github.com>
2021-09-10 06:09:27 +0500

    Update trie.py

--- Blind_75/trie.py
+++ Blind_75/trie.py
@@ -48,3 +48,46 @@ class Trie:
             node = node[letter]
         
         return True
+
+# 2. medium - Design Add and Search Words Data Structure
+class WordDictionary:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = {}
+        self.Symbol = '*'
+    
+    # trie
+    def addWord(self, word: str) -> None:
+        node = self.root
+        for letter in word:
+            if letter not in node:
+                node[letter] = {}
+            node = node[letter]
+        node[self.Symbol] = '*'
+        
+
+    def search(self, word: str, node=None) -> bool:
+        node = self.root
+        def search_inner(word, node):
+            
+            for idx in range(len(word)):
+                letter = word[idx]
+                # if '.' is met: explore all letters
+                if letter == '.':
+                    for i in node:
+                        # [a] - add; [a.] - search => False
+                        # if we remove `i != '*'` then we won't
+                        # catch this case as we have end, but `.`
+                        # still wants to search further
+                        if i != '*' and search_inner(word[idx + 1:], node[i]):
+                            return True
+                if letter not in node:
+                    return False
+                node = node[letter]
+        
+            return self.Symbol in node
+        
+        return search_inner(word, node)
