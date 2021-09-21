cc165804338fc6ed19a8fe0512ed6088e772a423
Daniil Slobodeniuk <76875535+SleeplessChallenger@users.noreply.github.com>
2021-09-21 10:46:13 +0500

    Update dfs_questions.py

--- graph_explore/dfs_questions.py
+++ graph_explore/dfs_questions.py
@@ -173,3 +173,56 @@ class Solution:
                 visited[curr].neighbors.append(temp)
 
         return visited[node]
+
+ # 4. Reconstruct Itinerary
+class Solution:
+    def __init__(self):
+        self.path = []
+    
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        flights = {}
+        
+        # as graph is directed
+        # => no bi-directional paths
+        for t1, t2 in tickets:
+            if t1 not in flights:
+                flights[t1] = []
+                
+            flights[t1].append(t2)
+
+        visited = {}
+        for k, v in flights.items():
+            v.sort()
+            visited[k] = [False for _ in range(len(v))]
+        
+        base_check = len(tickets) + 1
+        routes = ['JFK']
+        self.dfs('JFK', routes, base_check, flights, visited)
+        
+        return self.path
+        
+        
+    def dfs(self, curr, routes, base_check, flights, visited):
+        if len(routes) == base_check:
+            self.path = routes
+            return True
+        
+        # deadlock
+        if curr not in flights:
+            return False
+        
+        for idx in range(len(flights[curr])):
+            if not visited[curr][idx]:
+                visited[curr][idx] = True
+                
+                next_airport = flights[curr][idx]
+                routes += [next_airport]
+                result = self.dfs(next_airport, routes, base_check,
+                                    flights, visited)
+                
+                if result:
+                    return True
+                routes.pop()
+                visited[curr][idx] = False
+
+        # return False
