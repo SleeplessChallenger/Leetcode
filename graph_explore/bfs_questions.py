dab02a0975bbe77b8d10e8e0e2f0adc1bd5738d8
Daniil Slobodeniuk <76875535+SleeplessChallenger@users.noreply.github.com>
2021-09-25 16:06:48 +0500

    Update bfs_questions.py

--- graph_explore/bfs_questions.py
+++ graph_explore/bfs_questions.py
@@ -79,6 +79,9 @@ class Solution:
 
     # bfs
     def connect(self, root: 'Node') -> 'Node':
+    	'''
+    	by default `.next` = None
+    	'''
         if not root:
             return root
         
@@ -100,3 +103,151 @@ class Solution:
                     queue.append(node.right)
         
         return root
+
+# 4. Shortest Path in Binary Matrix
+class Solution:
+    # outer loop: O(n); inner loop: O(1) as max 8 directions
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        if grid[0][0] == 1:
+            return -1
+        
+        queue = [[0, 0]]
+        grid[0][0] = 1
+        
+        while len(queue) != 0:
+            path = queue.pop(0)
+            value = grid[path[0]][path[1]]
+            if path[0] == len(grid) - 1 and path[1] == len(grid[0]) - 1:
+                return value
+            
+            result = self.getAdjacent(path, grid)
+            for r in result:
+                row, col = r
+                grid[row][col] = value + 1
+                queue.append([row, col])
+            
+        return -1
+        
+    def getAdjacent(self, path, grid):
+        result = []
+        r, c = path
+        
+        if r > 0:
+            if grid[r - 1][c] == 0:
+                result.append([r - 1, c])
+
+        if c > 0:
+            if grid[r][c - 1] == 0:
+                result.append([r, c - 1])
+
+        if r + 1 <= len(grid) - 1:
+            if grid[r + 1][c] == 0:
+                result.append([r + 1, c])
+
+        if c + 1 <= len(grid[0]) - 1:
+            if grid[r][c + 1] == 0:
+                result.append([r, c + 1])
+
+        if r > 0 and c > 0:
+            if grid[r - 1][c - 1] == 0:
+                result.append([r - 1, c - 1])
+
+        if r > 0 and c + 1 <= len(grid[0]) - 1:
+            if grid[r - 1][c + 1] == 0:
+                result.append([r - 1, c + 1])
+
+        if r + 1 <= len(grid) - 1 and c > 0:
+            if grid[r + 1][c - 1] == 0:
+                result.append([r + 1, c - 1])
+
+        if r + 1 <= len(grid) - 1 and c + 1 <= len(grid[0]) - 1:
+            if grid[r + 1][c + 1] == 0:
+                result.append([r + 1, c + 1])
+        
+        return result
+
+# 5. N-ary Tree Level Order Traversal
+class Solution:
+    def levelOrder(self, root: 'Node') -> List[List[int]]:
+    	if root is None:
+    		return []
+
+    	result = []
+    	queue = [root]
+    	while len(queue) != 0:
+    		levels = []
+    		# python will iterate number of
+            # times which initially was
+            # specified despite further changes
+    		for _ in range(len(queue)):
+    			node = queue.pop(0)
+    			levels.append(node.val)
+    			# extend will remove []
+                # from .children array whilst
+                # append() will keep them
+    			queue += node.children
+    			# or .extend()
+
+    		result.append(levels)
+
+    	return result
+
+# 6. Rotting Oranges
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+    	queue, fresh = self.check_rotten(grid)
+
+    	queue.append((-1, -1))
+    	minutes = -1
+    	while len(queue) != 0:
+    		node = queue.pop(0)
+    		if node[0] == -1:
+    			minutes += 1
+    			if len(queue):
+    				queue.append((-1, -1))
+    		else:
+    			if grid[node[0]][node[1]] != 2:
+    				continue
+
+    			result = self.helper(grid, node[0], node[1])
+
+    			for r in result:
+    				row, col = r
+    				grid[row][col] = 2
+    				queue.append((row, col))
+    				fresh -= 1
+
+    	return minutes if fresh == 0 else -1
+
+    def helper(self, grid, r, c):
+        result = []
+        if r > 0:
+            if grid[r - 1][c] == 1:
+                result.append((r - 1, c))
+        
+        if c > 0:
+            if grid[r][c - 1] == 1:
+                result.append((r, c - 1))
+        
+        if r + 1 <= len(grid) - 1:
+            if grid[r + 1][c] == 1:
+                result.append((r + 1, c))
+        
+        if c + 1 <= len(grid[0]) - 1:
+            if grid[r][c + 1] == 1:
+                result.append((r, c + 1))
+        
+        return result
+
+    def check_rotten(self, grid):
+    	queue = []
+    	fresh = 0
+    	for i in range(len(grid)):
+    		for j in range(len(grid[i])):
+    			value = grid[i][j]
+    			if value == 2:
+    				queue.append((i, j))
+    			elif value == 1:
+    				fresh += 1
+
+    	return queue, fresh
