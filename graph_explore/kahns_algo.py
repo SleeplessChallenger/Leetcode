75e69e53c5951ff4c45a550514b4422ba3d3ab6e
Daniil Slobodeniuk <76875535+SleeplessChallenger@users.noreply.github.com>
2021-10-01 17:20:27 +0500

    Update kahns_algo.py

--- graph_explore/kahns_algo.py
+++ graph_explore/kahns_algo.py
@@ -108,5 +108,239 @@ class Node:
         
         return result
 
+# 2. Alien Dictionary
+class Solution:
+    def alienOrder(self, words: List[str]) -> str:
+        # not a valid edge case as
+        # input can be one word
+        # and also we may have 'aba' -> 'ab'
+        # => simple list() + reverse() doesn't work
+        
+        # if len(words) < 2:
+        #     return ''.join(list(reversed(words[0])))
+        
+        graph = {letter: [] for word in words for letter in word}
+        
+        for i in range(1, len(words)):
+            prev = words[i - 1]
+            curr = words[i]
+             
+            result = self.compare(prev, curr)
+            if not result:
+                return ""
+            
+            if result == 'Similar':
+                continue
+            
+            letterA, letterB = result
+            graph[letterA].append(letterB)
+        
+        result = []
+        visited = {} # False: visited, True: visitng
+        
+        for letter in graph:
+            if self.dfs(letter, visited, graph, result):
+                return ""
+        
+        return ''.join(list(reversed(result)))
+    
+    def dfs(self, letter, visited, graph, result):
+        if letter in visited:
+            return visited[letter]
+        
+        visited[letter] = True
+        
+        for vertex in graph[letter]:
+            if self.dfs(vertex, visited, graph, result):
+                return True
+        
+        visited[letter] = False
+        result.append(letter)
+        
+            
+    
+    def compare(self, a, b):
+        min_length = min(len(a), len(b))
+        
+        # not simply one > the other,
+        # but if first is bigger AND
+        # prefix of both is similar
+        if len(a) > len(b) and a[:min_length] == b[:min_length]:
+            return False
+        
+        for i in range(min_length):
+        # as we may face errors otherwise
+            letA = a[i]
+            letB = b[i]
+            
+            if letA != letB:
+                return [letA, letB]
+        
+        return 'Similar'
 
+# 3. Minimum Height Trees
+class Solution:
+    def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
+        '''
+        1. find such nodes that are overall close to
+            all the other nodes. Ex: leaf nodes are
+            peripheral and root is centoid.
+            
+            !! For tree-like graph - max 2 centroids. If number
+            of verticies is even - 2 centroids at max. If number
+            of verticies is odd - 1 centroid at max.
+        
+        2. Image there're more than 2 centroids => cycles
+            which contradicts our condition.
+        
+        3. Trim out from outer level (leaf nodes) to inner
+            level till there're 2 nodes left => centroids
+        '''
+        if n <= 2:
+            return [i for i in range(n)]
+        
+        graph = [[] for _ in range(n)]
+        
+        for e1, e2 in edges:
+            graph[e1].append(e2)
+            graph[e2].append(e1)
+        
+        queue = []
+        for i in range(n):
+            if len(graph[i]) == 1:
+                queue.append(i)
+        
+        count = n
+        while count > 2:
+            # cannot len(graph) as we don't
+            # remove keys, only pop() from values
+            temp = []
+            count -= len(queue)
+            
+            while len(queue) != 0:
+                node = queue.pop(0)
+                
+                adjacent = graph[node].pop(0)
+                graph[adjacent].remove(node)
+                # 2 above pop() from graph
+                
+                if len(graph[adjacent]) == 1:
+                    temp.append(adjacent)
+            
+            queue = temp
+        
+        return queue
+
+# 4. Parallel Courses
+# with class
+class Solution:
+    def minimumSemesters(self, n: int, relations: List[List[int]]) -> int:
+        '''
+        1. create graph and populate with prereqs
+        2. Don't forget to increase +1 as we have
+            from 1 to n, not 0 to n - 1
+        '''
+        graph = self.createGraph(n, relations)
+        return self.findCount(graph)
+    
+    def createGraph(self, n, relations):
+        graph = CourseClass(n)
+        
+        for curr, depend in relations:
+            graph.addVertex(curr, depend)
+        
+        return graph
+    
+    def findCount(self, graph):
+        nodes_start = list(filter(
+            lambda x: x.prepreqsCount == 0,
+            graph.verticies
+        ))
+        
+        count = 0
+        temp = []
+        
+        while len(nodes_start) != 0:
+            node = nodes_start.pop(0)
+            self.remove_depend(node, temp)
+            
+            if not len(nodes_start):
+                nodes_start = temp
+                temp = []
+                count += 1
+        
+        wrong_result = any(x.prepreqsCount for x in graph.verticies)
+        return -1 if wrong_result else count
+    
+    def remove_depend(self, node, temp):
+        while len(node.depends) != 0:
+            vertex = node.depends.pop(0)
+            vertex.prepreqsCount -= 1
+            if vertex.prepreqsCount == 0:
+                temp.append(vertex)
+  
+    
+class CourseClass:
+    def __init__(self, courses):
+        self.verticies = []
+        self.graph = {}
+        for course in range(1, courses + 1):
+            self.insert(course)
+    
+    def insert(self, course):
+        node = Course(course)
+        self.verticies.append(node)
+        self.graph[course] = node
+    
+    def getNode(self, course):
+        return self.graph[course]
+    
+    def addVertex(self, curr_course, next_course):
+        currNode = self.getNode(curr_course)
+        nextNode = self.getNode(next_course)
+        currNode.depends.append(nextNode)
+        nextNode.prepreqsCount += 1
+    
+
+class Course:
+    def __init__(self, course):
+        self.course = course
+        self.depends = []
+        self.prepreqsCount = 0
+
+# without class
+    def minimumSemesters(self, n: int, relations: List[List[int]]) -> int:
+        queue = []
+        graph = {}
+        in_count = {}
+        
+        for i in range(1, n + 1):
+            graph[i] = []
+            in_count[i] = 0
+        
+        for e1, e2 in relations:
+            graph[e1].append(e2)
+            in_count[e2] += 1
+        
+        for vertex in in_count:
+            if in_count[vertex] == 0:
+                queue.append(vertex)
+        
+        step = 0
+        courses = 0
+        
+        while len(queue) != 0:
+            step += 1
+            temp = []
+            for node in queue:
+                courses += 1
+                adjacent = graph[node]
+                for a in adjacent:
+                    in_count[a] -= 1
+                    if in_count[a] == 0:
+                        temp.append(a)
+            
+            queue = temp
+        
+        return step if courses == n else -1
 
