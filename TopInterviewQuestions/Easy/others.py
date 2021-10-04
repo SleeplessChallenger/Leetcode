cdd46b6d4a37c243ce2bf028ce9a91775072cda7
Daniil Slobodeniuk <76875535+SleeplessChallenger@users.noreply.github.com>
2021-10-04 10:35:05 +0500

    Update others.py

--- TopInterviewQuestions/Easy/others.py
+++ TopInterviewQuestions/Easy/others.py
@@ -14,6 +14,21 @@ class Solution:
             dp.append(curr)
         
         return dp
+# ver. 2
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        result = [[1]]
+        count = 0
+        while count < numRows - 1:
+            count += 1
+            curr = [1]
+            for i in range(1, count):
+                curr.append(result[count-1][i-1] + 
+                           result[count-1][i])
+            curr.append(1)
+            result.append(curr)
+        
+        return result
 
 # 2. Hamming Distance
 class Solution:
