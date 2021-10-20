afaf3343deec491a3af7182ea1dfe82525368955
Daniil Slobodeniuk <76875535+SleeplessChallenger@users.noreply.github.com>
2021-10-20 09:25:59 +0500

    Update design.py

--- TopInterviewQuestions/Easy/design.py
+++ TopInterviewQuestions/Easy/design.py
@@ -28,6 +28,55 @@ class MinStack:
     def getMin(self) -> int:
         return self.MinStack[-1]['min']
 
+# another ver.
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+        
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if not self.min_stack:
+            self.min_stack.append({'min': val})
+        else:
+            curr_min = self.min_stack[-1]['min']
+            result = min(curr_min, val)
+            self.min_stack.append({'min': result})
+
+    def pop(self) -> None:
+        '''
+        take list: [-2,0,-3]
+        
+        1. When -2 is pushed: it is in boths stacks in __inti__
+        2. When 0 is pushed, 0 is in self.stack, but -2 again
+            pushed to self.min_stack. Hence when we .pop() =>
+            not only 0 is removed from stack, but that additional
+            -2 in self.min_stack (as we have one more -2 before)
+            is also removed
+        '''
+        self.min_stack.pop()
+        self.stack.pop()
+        
+    '''
+    if we don't have cases when
+    two func below are called 
+    and stack being empty
+    '''
+    def top(self) -> int:
+        return self.stack[-1]
+    
+    def getMin(self) -> int:
+        return self.min_stack[-1]['min']
+        
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
+
 # 2. Shuffle an Array
 class Solution:
 
