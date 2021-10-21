5a02feee445d4a3b17dfe1e7703b88bf164e6426
Daniil Slobodeniuk <76875535+SleeplessChallenger@users.noreply.github.com>
2021-10-21 11:28:23 +0500

    Update graph.py

--- Blind_75/graph.py
+++ Blind_75/graph.py
@@ -1,5 +1,6 @@
 # medium chunk
 # 1. medium - Number of Islands
+# DFS
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         explored = [[False for col in range(len(grid[0]))] for row in grid]
@@ -42,6 +43,72 @@ class Solution:
             stack.append([i + 1, j])
         if j < len(grid[i]) - 1 and not explored[i][j + 1]:
             stack.append([i, j + 1])
+
+    # Union Find
+    def numIslands(self, grid: List[List[str]]) -> int:
+        rows = len(grid)
+        cols = len(grid[0])
+
+        ones = self.count_ones(grid)
+        
+        uf = UnionFind(rows*cols, ones)
+        
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == '0':
+                    continue
+                
+                index = i * cols + j
+
+                # `rows - 1` is last idx =>
+                # we use `<` to allow one step
+                if i < rows - 1 and grid[i+1][j] == '1':
+                    uf.union(index, index + cols)
+
+                if j < cols - 1 and grid[i][j+1] == '1':
+                    uf.union(index, index + 1)
+                
+        
+        return uf.count
+                
+    def count_ones(self, grid):
+        count = 0
+        
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                if grid[i][j] == '1':
+                    count += 1
+     
+        return count
+        
+
+class UnionFind:
+    def __init__(self, size, ones_count):
+        self.root = [i for i in range(size)]
+        self.rank = [1 for _ in self.root]
+        self.count = ones_count
+    
+    def find(self, vertex):
+        if vertex == self.root[vertex]:
+            return vertex
+        self.root[vertex] = self.find(self.root[vertex])
+        return self.root[vertex]
+    
+    def union(self, v1, v2):
+        root1 = self.find(v1)
+        root2 = self.find(v2)
+        
+        if root1 != root2:
+            if self.rank[root1] > self.rank[root2]:
+                self.root[root2] = root1
+            elif self.rank[root1] < self.rank[root2]:
+                self.root[root1] = root2
+            else:
+                self.root[root2] = root1
+                self.rank[root1] += 1
+            
+            self.count -= 1
+            
 
 # 2 medium - Pacific Atlantic Water Flow
 class Solution:
@@ -135,3 +202,109 @@ class Solution:
             
             self.dfs(grid, length, height, visited, new_row, new_col)
    
+
+# 3. Course Schedule
+class Solution:
+    # class structure
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        graph = self.create_graph(numCourses, prerequisites)
+        return self.can_finish(graph)
+        
+    def create_graph(self, num, prerequisites):
+        graph = Courses(num)
+        
+        for main, prereq in prerequisites:
+            graph.update(prereq, main)
+        
+        return graph
+    
+    def can_finish(self, graph):
+
+        for vertex in graph.verticies:
+            isCycle = self.dfs(vertex, graph)
+            if isCycle:
+                return False
+        
+        return True
+    
+    def dfs(self, vertex, graph):
+        if vertex.visiting:
+            return True
+        if vertex.visited:
+            return False
+        
+        vertex.visiting = True
+        
+        for prereq in vertex.prereqs:
+            isCycle = self.dfs(prereq, graph)
+            if isCycle:
+                return True
+        
+        vertex.visited = True
+        vertex.visiting = False
+        return False
+        
+
+class Courses:
+    def __init__(self, courses):
+        self.graph = {}
+        self.verticies = []
+        for course in range(courses):
+            self.insert(course)
+            
+    def insert(self, course):
+        course_node = Course(course)
+        self.verticies.append(course_node)
+        self.graph[course] = course_node
+    
+    def get_node(self, course):
+        return self.graph[course]
+    
+    def update(self, a, b):
+        node_a = self.get_node(a)
+        node_b = self.get_node(b)
+        node_a.prereqs.append(node_b)
+
+        
+class Course:
+    def __init__(self, course):
+        self.course = course
+        self.prereqs = []
+        self.visited = False
+        self.visiting = False
+        
+    # adjacency list
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        if numCourses == 1:
+            return True
+        
+        graph = {}
+        inDegree = {}
+        
+        for i in range(numCourses):
+            graph[i] = []
+            inDegree[i] = 0
+        
+        for main, prereq in prerequisites:
+            graph[main].append(prereq)
+            inDegree[prereq] += 1
+        
+        queue = []
+        for node in inDegree:
+            if inDegree[node] == 0:
+                queue.append(node)
+        
+        while len(queue) != 0:
+            node = queue.pop(0)
+            
+            for vertex in graph[node]:
+                inDegree[vertex] -= 1
+                if inDegree[vertex] == 0:
+                    queue.append(vertex)
+            
+            
+        for vertex in inDegree:
+            if inDegree[vertex] != 0:
+                return False
+            
+        return True
