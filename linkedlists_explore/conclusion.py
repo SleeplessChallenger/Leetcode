0c302530ce2d92e8bd9e94a9c1a44e780616aed1
Daniil Slobodeniuk <76875535+SleeplessChallenger@users.noreply.github.com>
2021-11-12 08:02:10 +0300

    Update conclusion.py

--- linkedlists_explore/conclusion.py
+++ linkedlists_explore/conclusion.py
@@ -292,4 +292,50 @@ class Solution:
         
         return new_head
 
+# 5. Rotate List
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        if head is None:
+            return None
+        
+        length = 1
+        # here we use 1 as we want to stop
+        # at last node and have it as tail,
+        # but also to include it in length
+        tail = head
+        
+        # find tail
+        while tail.next:
+            length += 1
+            tail = tail.next
+ 
+        offset = abs(k) % length
+        # if k > length => diff between length and k
+        # else: k
+        
+        if offset == 0:
+            return head
+        
+        new_tail_index = length - offset if k > 0 else offset
+        # if k is negative:
+        # a) abs(k) < length => abs(k) % length = abs(k)
+        # b) abs(k) > length => abs(k) % length = length
+        
+        # + keep in mind that index starts at 0
+        
+        count = 1
+        newTail = head
+        while count != new_tail_index:
+            newTail = newTail.next
+            count += 1
 
+        newHead = newTail.next
+        newTail.next = None
+        
+        tail.next = head
+        return newHead
